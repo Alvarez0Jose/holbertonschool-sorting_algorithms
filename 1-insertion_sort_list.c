7435d56843662bdd7a406c9fee13d9ce5225dc3e
Alvarez0Jose <josealejandro220@gmail.com>
2023-05-24 13:21:17 +0000

    fixing some syntax errors

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,10 +9,11 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *key, *prev;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current, *key, *prev;
 
 	current = (*list)->next;
 	while (current != NULL)
